b30e3439909305d0a2b427693d0c99465508569c
Diego Valero <diego27jay@gmail.com>
2022-01-11 16:55:31 -0700

    commit tree 9c534db3595b53fe552a8a53b1f54c6c1aa19f19

--- index.js
+++ index.js
@@ -3,6 +3,31 @@
 //HQ on 42nd
 
 function distanceFromHqInBlocks(destination) {
-    let hq = 42;
-    return Math.abs(destination - hq)
+    const hq = 42;
+    return Math.abs(hq - destination)
+}
+
+function distanceFromHqInFeet(destination) {
+    const blockInFeet = 264; 
+    const blocks = distanceFromHqInBlocks(destination);
+    return blockInFeet * blocks;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(start - destination);
+    return blockInFeet * blocks;
+}
+
+const cents = 0.02
+function calculatesFarePrice(start, destination) {
+    const feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 399) {
+        return 0;
+    } else if (feet <= 2000) {
+        return cents * (feet - 400);
+    } else if (feet <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
 }
